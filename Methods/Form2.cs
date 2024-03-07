949821fb0e7cf61f84f65f3f8c0263d8b65f34b5
Sidarciftcioglu <Sidarciftcioglu47@gmail.com>
2024-03-07 10:10:02 +0300

    Add project files.

---
+++ Methods/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Methods
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int Sum(int s1,int s2)
+        {
+            int s3 = s1 + s2;
+            return s3;
+        }
+        int Multi(int m1,int m2,int m3)
+        {
+            int m4 = m1 * m2 * m3;
+            return m4;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label1.Text = Sum(3,4).ToString();
+            label1.Text=Multi(4,6,8).ToString();
+            
+        }
+    }
+}
---
+++ Methods/Form3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Methods
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        int Cube(int n1)
+        {
+            int n2 = n1 * n1 * n1;
+            return n2;
+        }
+        int Sum(int k1,int k2)
+        {
+            return k1 + k2;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int number = Convert.ToInt16(textBox1.Text);
+            //label1.Text = Cube(number).ToString();
+            
+            int number1=Convert.ToInt32(textBox1.Text);
+            int number2=Convert.ToInt32(textBox2.Text);
+            label1.Text = Sum(number2, number1).ToString();
+        }
+    }
+}
